d5ef2bace23b57fd467a994451263bb3fd1a62dc
Hirohi Maruyama <maruyama@near.co.jp>
2016-05-24 09:25:28 +0900

    a

---
+++ appengine/multitenancy/appengine_config.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+"""
+`appengine_config.py` is automatically loaded when Google App Engine
+starts a new instance of your application. This runs before any
+WSGI applications specified in app.yaml are loaded.
+"""
+
+import os
+from google.appengine.ext import vendor
+
+# Third-party libraries are stored in "lib", vendoring will make
+# sure that they are importable by the application.
+#vendor.add('lib')
+vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
+
+import os
+from google.appengine.api.app_identity import app_identity
+def namespace_manager_default_namespace_for_request():
+    """ デフォルトのネームスペースを決める
+    http://yellow-1317.appspot.com/datastore >> yellow-1317.appspot.com
+    http://near109.com/datastore >> near109.com
+    """
+    return os.environ.get('HTTP_HOST', '')
